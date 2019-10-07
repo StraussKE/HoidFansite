4e9aed18a735bdb0d09a0f6ee09559272bb0020a
Katie Strauss <straussk@my.lanecc.edu>
2019-10-06 20:14:17 -0700

    views added

--- Lab1-HoidFansite/Controllers/SourcesController.cs
+++ Lab1-HoidFansite/Controllers/SourcesController.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Lab1_HoidFansite.Models;
 
 namespace Lab1_HoidFansite.Controllers
 {
-    public class HomeController : Controller
+    public class SourcesController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
             return View();
         }
