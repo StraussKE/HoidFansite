0c845992a28e21b79bab3a4a625f95215c3636cf
Katie Strauss <straussk@my.lanecc.edu>
2019-11-26 17:21:49 -0800

    where did my sources go?

--- HoidFansite/Controllers/HomeController.cs
+++ HoidFansite/Controllers/HomeController.cs
@@ -15,5 +15,35 @@ namespace HoidFansite.Controllers
         {
             return View();
         }
+
+        // This is an example of using RedirectToRouteResult and returning a RedirectToRoute
+        public RedirectToRouteResult DemoRedirect() =>
+            RedirectToRoute(new
+            {
+                controller = "Example",
+                action = "Index",
+                ID = "MyID"
+            });
+
+        // This is an example of using ObjectResult and returning Ok
+        public ObjectResult DemoObjectResult() => Ok(new string[] { "Kaladin", "Dalinar", "Wit" });
+
+        // this is an example of hard coding a StatusCodeResult and returning NotFound
+        public StatusCodeResult DemoNotFound() => NotFound();
+
+        public ActionResult DemoValidateAddition(Book book)
+        {
+            if (book.Author != "Brandon Sanderson")
+            {
+                ModelState.AddModelError(
+                    "NonCanon",
+                    "Only books written by Brandon Sanderson are considered canon in the Cosmere.");
+            }
+            if (ModelState.IsValid)
+            {
+                BookRepository.AddBook(book);
+                return RedirectToAction("Book")
+            }
+        }
     }
 }
